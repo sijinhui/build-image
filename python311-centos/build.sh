2485417f3ea1d3d4ca6274bd0085bd3d07fa112d
sijinhui <62741773+sijinhui@users.noreply.github.com>
2025-04-28 10:59:00 +0800

    修改文档字体

--- python311-centos/build.sh
+++ python311-centos/build.sh
@@ -5,7 +5,7 @@ mkdir -p /app/output
 
 # 压缩 Python 安装目录
 cd /usr/local
-tar czf /app/output/python3.11-centos7.tar.gz lib/python3.11 lib/python3.11.* bin/python3.11 bin/pip3.11
+tar czf /app/output/python3.11-centos7.tar.gz lib/python3.11 lib/python3.11* bin/python3.11 bin/pip3.11
 
 # 输出文件大小信息
 ls -lh /app/output/python3.11-centos7.tar.gz
